db28959aa756c32f9988236bbb28bf1e30170d9c
MAKCIMELIANO <makcimeliano@gmail.com>
2022-09-05 14:41:48 +0300

    push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div style={{ maxWidth: 1170, margin: "0 auto", padding: 20 }}>
+        hello worold
+      </div>
+    );
+  }
 }
-
-export default App;
